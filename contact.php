6efc8fcccb39663942c3d9665879bfcb7d590b33
Devviv <aamehole@gmail.com>
2023-04-28 18:36:47 +0000

    Le site finalement près et passe à l'hébergement final!

---
+++ contact.php
@@ -0,0 +1,4 @@
+<?php echo "Nom = ".$_POST['name']?><br>
+<?php echo "Email = ".$_POST['email'] ?><br>
+<?php echo "Sujet = ".$_POST['subject'] ?><br>
+<?php echo "Message = ".$_POST['message'] ?><br>
