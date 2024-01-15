eaf817501fdcf1ba400547da327bc1a60fc76bfe
Vivien <aamehole@gmail.com>
2024-01-15 00:51:29 +0000

    Mise à jour de quesques textes

--- contact.php
+++
@@ -1,4 +0,0 @@
-<?php echo "Nom = ".$_POST['name']?><br>
-<?php echo "Email = ".$_POST['email'] ?><br>
-<?php echo "Sujet = ".$_POST['subject'] ?><br>
-<?php echo "Message = ".$_POST['message'] ?><br>
